41edf9d3e8bea29bbf761e3fb46dc48cfbf176de
brensch <brendolini@protonmail.com>
2022-09-03 15:41:37 -0700

    removed print from background task

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -23,10 +23,6 @@ firebase.initializeApp({
 const messaging = firebase.messaging()
 
 messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload,
-  )
   // Customize notification here
   const notificationTitle = payload.data.title
   const notificationOptions = {
